ed17544225589597df3c432eab51e5e47857216b
Stas SUȘCOV <stas@net.utcluj.ro>
2011-05-16 02:13:37 +0300

    Courses, full CRUD ready, improved user level security, submenus system ready... gods added

--- joodle/app/controllers/Courses.java
+++ joodle/app/controllers/Courses.java
@@ -1,9 +1,10 @@
 package controllers;
 
 import java.util.List;
-import models.Course;
 import play.mvc.*;
 import play.mvc.results.NotFound;
+
+import models.*;
 
 /**
  * Courses controller
@@ -35,7 +36,58 @@ public class Courses extends Controller {
      * Render course editor for course ID
      * @param id, the course ID
      */
-    public static void edit( Integer id ) {
+    public static void edit( Long id ) {
+        Application.currentUserCan( 1 );
+        
+        Course course = Course.findById( id );
+        if( course == null )
+            throw new NotFound(null);
+        
+        render( course );
+    }
+    
+    /**
+     * Render course editor for adding new course
+     */
+    public static void add() {
+        Application.currentUserCan( 1 );
         render();
+    }
+    
+    /**
+     * Handle POST form to update a course
+     * @param id, course to be updated
+     */
+    public static void update( Long id ) {
+        Application.currentUserCan( 1 );
+        
+        String title = params.get( "course[title]", String.class );
+        String content = params.get( "course[content]", String.class );
+        Long old_id = params.get( "course[title]", Long.class );
+        Course course = Course.findById( id );
+        if( !id.equals(old_id) && course != null ) {
+            course.setTitle(title);
+            course.setContent(content);
+            course.save();
+        }
+        edit(id);
+    }
+    
+    /**
+     * Handle POST form to create a course
+     */
+    public static void create() {
+        Application.currentUserCan( 1 );
+        
+        String author = session.get("userEmail");
+        String title = params.get( "course[title]", String.class );
+        String content = params.get( "course[content]", String.class );
+        
+        if( title.length() > 0 && content.length() > 0 ) {
+            Course course = new Course(title, content, author);
+            course.save();
+        }
+        
+        index();
     }
 }
